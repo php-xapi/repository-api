ecc017f84ed4c913487fd4d0672df0d916d6844e
Jérôme Parmentier <jerome.parmentier@acensi.fr>
2017-03-06 17:20:19 +0100

    Remove useless constant

--- Test/Functional/ActivityRepositoryTest.php
+++ Test/Functional/ActivityRepositoryTest.php
@@ -20,8 +20,6 @@ use XApi\Repository\Api\ActivityRepositoryInterface;
  */
 abstract class ActivityRepositoryTest extends \PHPUnit_Framework_TestCase
 {
-    const UUID_REGEXP = '/^[a-f0-9]{8}-[a-f0-9]{4}-[1-5][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/i';
-
     /**
      * @var ActivityRepositoryInterface
      */
